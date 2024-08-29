10e649c201b7ae1d1bc05521524e5f1591cc07f3
vijaykarra <vijay.karra@xelpmoc.in>
2024-08-29 09:36:34 +0530

    Function creation using loop

---
+++ Function_creation/loop.py
@@ -0,0 +1,5 @@
+def counter(n):
+    for i in range(n, 12):
+        n = n+1
+        print(n)
+    return n
---
+++ counter_function/output.py
@@ -0,0 +1,10 @@
+from Function_creation.loop import counter
+
+
+def counter_functions():
+    n = 9
+    counter(n)
+
+
+if __name__ == "__main__":
+    counter_functions()
